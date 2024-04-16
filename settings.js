a5b5f4581e7e91a84672874e372a40b84d0c3eb5
johncolins21 <85611520+johncolins21@users.noreply.github.com>
2024-04-16 18:09:01 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -58,7 +58,7 @@ global.ownername = process.env.SUDO_NAME || 'Salman Ahmad' //ur owner name
 //________________________________________________________________________________________________________
 global.websitex = process.env.YT_LINK || "https://youtube.com/@s4salmanyt"
 //________________________________________________________________________________________________________
-global.wagc = process.env.GL_PUSH || "https://whatsapp.com/channel/0029VaINgEZK0IBaK1nDtE3f"
+global.wagc = process.env.GL_PUSH || ""
 //________________________________________________________________________________________________________
 global.themeemoji = process.env.GL_EMOJI || '🏮'
 //________________________________________________________________________________________________________
